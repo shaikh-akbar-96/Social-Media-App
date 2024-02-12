3ab34b5b9311fbb3e258e251b392b3e4bf43b0f0
shaikh-akbar-96 <akbarhshaikh1996@gmail.com>
2024-02-13 01:26:36 +0530

    creating social media app using react tailwind css and sanity.io cms

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,18 @@
+// user.js
+export default {
+  name: 'user',
+  type: 'document',
+  title: 'User',
+  fields: [
+    {
+      name: 'userName',
+      type: 'string',
+      title: 'User Name',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+  ],
+}
---
+++ shareme_frontend/src/Layout/Root.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import Header from "../components/Header/Header";
+import { Outlet } from "react-router-dom";
+// import Footer from "../components/Footer/Footer";
+
+const Root = () => {
+  return;
+  // <div>Hi</div>;
+};
+
+export default Root;
