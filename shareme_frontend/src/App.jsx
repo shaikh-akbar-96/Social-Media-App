2650e14fa35a5a25fbcbc47efb357ef41e3ab86c
shaikh-akbar-96 <akbarhshaikh1996@gmail.com>
2024-02-13 22:48:36 +0530

    bugs fixes

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+        <RouterProvider router={router} />;
+      </GoogleOAuthProvider>
+      ;
+    </>
+  );
 };
 
 export default App;
